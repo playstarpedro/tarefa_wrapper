17622bea8c8bfdbea8aad5d7beb117a41379caa1
pedroDev <playstarpedro@gmail.com>
2024-07-06 10:58:45 -0300

    first commit

---
+++ src/Wrapper.java
@@ -0,0 +1,10 @@
+public class Wrapper {
+
+    public static void main(String args[]) {
+        int num1 = 10;
+        System.out.println(num1);
+        Integer num2 = num1;
+        System.out.println(num2);
+
+    }
+}
